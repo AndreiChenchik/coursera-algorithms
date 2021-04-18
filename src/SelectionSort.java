0eaf1520e2e2919e1024119962dd42c27d0aa835
Andrei Chenchik <andrei@chenchik.me>
2021-04-18 20:35:26 +0200

    Sorting: Selection Sort implementation

---
+++ src/SelectionSort.java
@@ -0,0 +1,58 @@
+/* *****************************************************************************
+ *  Name:              Andrei Chenchik
+ *  Coursera User ID:  c27c0b79c8a674f5a98145672b4581b2
+ *  Last modified:     March 19, 2021
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int length = 20;
+        Double[] doubleArray = new Double[length];
+        for (int i = 0; i < length; i++) {
+            doubleArray[i] = StdRandom.uniform();
+            StdOut.print(doubleArray[i] + " ");
+        }
+        StdOut.println();
+
+        sort(doubleArray);
+        StdOut.println("Sorted: " + isSorted(doubleArray));
+
+        for (int i = 0; i < length; i++) {
+            StdOut.print(doubleArray[i] + " ");
+        }
+    }
+
+    public static <Item extends Comparable<Item>> void sort(Item[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (less(array[j], array[min])) {
+                    min = j;
+                }
+            }
+            exchange(array, i, min);
+        }
+    }
+
+    private static <Item extends Comparable<Item>> boolean isSorted(Item[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (less(array[i], array[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static <Item extends Comparable<Item>> boolean less(Item first, Item second) {
+        return first.compareTo(second) < 0;
+    }
+
+    private static <Item extends Comparable<Item>> void exchange(Item[] array, int first, int second) {
+        Item swap = array[first];
+        array[first] = array[second];
+        array[second] = swap;
+    }
+}
